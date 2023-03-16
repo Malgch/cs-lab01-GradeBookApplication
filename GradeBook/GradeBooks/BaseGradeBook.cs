b152aa9f4dd42448a0f439d3f702cdceef8851fd
Małgorzata Chudy <malgorzata.chudy@microsoft.wsei.edu.pl>
2023-03-16 23:48:17 +0100

    2 unit tests fixed

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -13,6 +14,8 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {
@@ -265,6 +268,51 @@ namespace GradeBook.GradeBooks
                              select type).FirstOrDefault();
             
             return JsonConvert.DeserializeObject(json, gradebook);
+        }
+    }
+
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            double allStudentsGrades = Students.Average (s => s.AverageGrade);
+            int numberOfStudents = 0;
+
+            foreach (var student in Students)
+            {
+                numberOfStudents++;
+            }
+
+
+
+                if (averageGrade < 95)
+                throw new InvalidOperationException("Invalid operation");
+
+            else if (averageGrade > 20)
+                return 'A';
+            else if (averageGrade > 40 && averageGrade <= 20)
+                return 'B';
+            else if (averageGrade > 60 && averageGrade <= 40)
+                return 'C';
+            else if (averageGrade > 80 && averageGrade <= 60)
+                return 'D';
+
+            else return 'F';
+
         }
     }
 }
